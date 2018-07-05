6f181da559c1b9f54a66ef3ed686ce9719f28684
Jim Crossley <jim@crossleys.org>
2018-07-04 20:14:55 -0400

    Make local work, too

--- bin/start.sh
+++ bin/start.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-exec minikube start --memory=8192 --cpus=4 \
-  --kubernetes-version=v1.10.0 \
-  --vm-driver=none \
-  --bootstrapper=localkube \
-  --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
-  --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
-  --extra-config=apiserver.admission-control="DenyEscalatingExec,LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+if [ "$CI" = "true" ]; then
+  exec minikube start --memory=8192 --cpus=4 \
+       --kubernetes-version=v1.10.0 \
+       --vm-driver=none \
+       --bootstrapper=localkube \
+       --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
+       --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
+       --extra-config=apiserver.admission-control="DenyEscalatingExec,LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+else
+  exec minikube start --memory=8192 --cpus=4 \
+       --kubernetes-version=v1.10.4 \
+       --vm-driver=kvm2 \
+       --bootstrapper=kubeadm \
+       --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
+       --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
+       --extra-config=apiserver.admission-control="DenyEscalatingExec,LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+fi
